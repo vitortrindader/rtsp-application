599959fe425b6158843ee4a05b564ae9953543bf
Vitor Trindade <85366315+vitortrindader@users.noreply.github.com>
2024-01-22 01:31:09 -0300

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,75 +1,51 @@
-from ultralytics import YOLO
-import time
 import streamlit as st
-import numpy as np
 import cv2
-from pytube import YouTube
-import settings
 import subprocess
 
-def create_interface(st_frame, frame,ecg_data):
+
+def create_interface(st_frame, frame, ecg_data, prediction):
 
     number_1 = ecg_data[-1] if ecg_data else 0
-    number_2 = 2
-    number_3 = 3
+    number_1 = round(number_1, 4)
+    number_2 = prediction[-1] if prediction else 0
+    if number_2 == 0:
+        number_2 = "Normal"
+    elif number_2 == 1:
+        number_2 = "Supraventricular"
+    elif number_2 == 2:
+        number_2 = "Ventricular"
+    elif number_2 == 3:
+        number_2 = "Fusion"
+    elif number_2 == 4:
+        number_2 = "Unknown"
 
-    height, width, _ = frame.shape
-
-    p1 = int(height / 28.33)
-    p2_w = int(width / 3.55)
-    p2_h = int(height / 2.05)
-
+    # Logic to show prediction
     overlay = frame.copy()
-
-    cv2.rectangle(overlay, (p1, p1), (p2_w, p2_h), (0, 0, 0), thickness=cv2.FILLED)
-
+    cv2.rectangle(overlay, (22, 22), (350, 100), (0, 0, 0), thickness=cv2.FILLED)
     cv2.addWeighted(overlay, 0.55, frame, 0.45, 0, frame)
-
     font = cv2.FONT_HERSHEY_DUPLEX
-    font_scale = 1.5
-    font_thickness = 3
-
-    p_w = int(width / 7.73)
-    p_h = int(height / 5.37)
-
-    text_1 = "HR"
-    text_color_1 = (113, 244, 164)
-    cv2.putText(frame, str(number_1), (p_w, p_h), font, font_scale, text_color_1, font_thickness)
-    p_w1 = p_w - 40
-    cv2.putText(frame, text_1, (p_w1, p_h), font, font_scale * 0.4, text_color_1, 1)
-
-    text_2 = "SpO2"
-    text_color_2 = (81, 249, 249)
-    p_h2 = p_h + 60
-    cv2.putText(frame, str(number_2), (p_w, p_h2), font, font_scale, text_color_2, font_thickness)
-    p_w2 = p_w - 60
-    cv2.putText(frame, text_2, (p_w2, p_h2), font, font_scale * 0.4, text_color_2, 1)
-
-    text_3 = "etCO2"
-    text_color_3 = (220, 246, 77)
-    p_h3 = p_h2 + 60
-    cv2.putText(frame, str(number_3), (p_w, p_h3), font, font_scale, text_color_3, font_thickness)
-    p_w3 = p_w - 60
-    cv2.putText(frame, text_3, (p_w3, p_h3), font, font_scale * 0.4, text_color_3, 1)
-
+    cv2.putText(frame, str(number_1), (65, 50), font, 1 * 0.7, (113, 244, 164), 1)
+    cv2.putText(frame, "ECG", (25, 50), font, 1 * 0.4, (113, 244, 164), 1)  # ECG
+    cv2.putText(frame, str(number_2), (110, 80), font, 1 * 0.7, (81, 249, 249), 1)
+    cv2.putText(frame, "PREDICTION", (25, 80), font, 1 * 0.4, (81, 249, 249), 1)  # Prediction
 
     st_frame.image(frame,caption='inteface',channels="BGR",use_column_width=True)
     return frame
 
 
-def play_rtsp_stream_modified(ecg_data):
+def play_rtsp_stream_modified(prediction, ecg_data):
     source_rtsp = st.sidebar.text_input("rtsp stream url:")
     st.sidebar.caption('Example URL: rtsp://admin:12345@192.168.1.210:554/Streaming/Channels/101')
     
     rtsp_output_url = st.sidebar.text_input("rtsp output url:")
     st.sidebar.caption('Example URL: rtsp://your_server_ip:554/live/stream')
 
-    if st.sidebar.button('Detect Objects'):
+    if st.sidebar.button('PLAY'):
         try:
             vid_cap = cv2.VideoCapture(source_rtsp)
             st_frame = st.empty()
             width, height = 640, 480
-            fps = 30
+            fps = 60
             vid_cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
             vid_cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
             vid_cap.set(cv2.CAP_PROP_FPS, fps)
@@ -91,9 +67,10 @@ def play_rtsp_stream_modified(ecg_data):
             ffmpeg_process = subprocess.Popen(ffmpeg_cmd, stdin=subprocess.PIPE)
 
             while vid_cap.isOpened():
-                success, image = vid_cap.read()
+                success, imagem = vid_cap.read()
+                image = cv2.resize(imagem, (640, 480))
                 if success:
-                    frame = create_interface(st_frame, image, ecg_data)
+                    frame = create_interface(st_frame, image, ecg_data, prediction)
                     ffmpeg_process.stdin.write(frame.tobytes())
                 else:
                     break
