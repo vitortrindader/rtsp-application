6d095509b52ca823c3c6499bea7feb9ff8c46dff
Vitor Trindade <85366315+vitortrindader@users.noreply.github.com>
2024-01-22 01:30:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,51 @@
 import paho.mqtt.client as mqtt
-import time
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+import streamlit as st
+import settings
+import helper
+from keras.models import load_model
 import numpy as np
 
-ecg_data = [0]          
+loaded_model = load_model('best_model.h5')
 
-def on_message(client, userdata, message):
+ecg_data = [0]          
+batch_size = 186
+batches = []
+prediction = []
+
+
+def predict_single_input(model, input_data):
+    input_data = np.reshape(input_data, (1, input_data.shape[0], 1))
+    model_response = model.predict(input_data)
+    predicted_class = np.argmax(model_response)
+    return predicted_class
+
+
+def process_batch(batch):
+    input_array = np.array(batch)
+    predicted_class = predict_single_input(loaded_model, input_array)
+    prediction.append(predicted_class)
+
+
+def on_message(client_, userdata, message):
 
     try:
         new_data = float(message.payload.decode())
-        ecg_data.append(new_data)     
+        ecg_data.append(new_data)
+
+        if len(ecg_data) >= batch_size:
+            current_batch = ecg_data[-batch_size:]
+            batches.append(current_batch)
+            process_batch(current_batch)
 
     except ValueError:
         print("Invalid data received.")
+
 
 client = mqtt.Client()
 
 client.on_message = on_message
 
-client.connect("127.0.0.1", 1883)
+client.connect("192.168.15.26", 1883)
 
 client.loop_start()
 
@@ -27,13 +53,6 @@ topic = "ambulance/ecg"
 
 client.subscribe(topic)
 
-#--------------------------------------------------------------------
-
-from pathlib import Path
-import PIL
-import streamlit as st
-import settings
-import helper
 
 st.set_page_config(
     page_title="Ambulance Monitoring",
@@ -47,7 +66,7 @@ st.title("Ambulance Monitoring")
 source_radio = 'RTSP'
 
 if source_radio == settings.RTSP:
-    helper.play_rtsp_stream_modified(ecg_data)
+    helper.play_rtsp_stream_modified(prediction,ecg_data)
 
 else:
     st.error("Please select a valid source type!")
